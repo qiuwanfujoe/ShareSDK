30eec0645bcd1f795763ef2e4cd4f7ccc618f2d9
qiuwf <qiuwf@Ctrip.com>
2016-02-03 21:57:01 +0800

    修改配置文件

--- ShareSDK/ViewController.m
+++ ShareSDK/ViewController.m
@@ -30,7 +30,12 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)sinaWeibo:(id)sender {
-    [self sinaWeiboPressed];
+    WBAuthorizeRequest *authRequest = [WBAuthorizeRequest request];
+    authRequest.redirectURI = kRedirectURI;
+    authRequest.scope = @"all";
+    
+    WBSendMessageToWeiboRequest *request = [WBSendMessageToWeiboRequest requestWithMessage:[self messageToShare] authInfo:authRequest access_token:nil];
+    [WeiboSDK sendRequest:request];
 }
 
 - (IBAction)weixinFriend:(id)sender
@@ -105,32 +110,16 @@
     QQApiSendResultCode sent = [QQApiInterface sendReq:req];
 }
 
-- (void)sinaWeiboPressed
-{
-    WBAuthorizeRequest *authRequest = [WBAuthorizeRequest request];
-    authRequest.redirectURI = kRedirectURI;
-    authRequest.scope = @"all";
-    
-    WBSendMessageToWeiboRequest *request = [WBSendMessageToWeiboRequest requestWithMessage:[self messageToShare] authInfo:authRequest access_token:nil];
-    request.userInfo = @{@"ShareMessageFrom": @"SendMessageToWeiboViewController",
-                         @"Other_Info_1": [NSNumber numberWithInt:123],
-                         @"Other_Info_2": @[@"obj1", @"obj2"],
-                         @"Other_Info_3": @{@"key1": @"obj1", @"key2": @"obj2"}};
-    //    request.shouldOpenWeiboAppInstallPageIfNotInstalled = NO;
-    [WeiboSDK sendRequest:request];
-}
-
-
 - (WBMessageObject *)messageToShare
 {
     WBMessageObject *message = [WBMessageObject message];
-        message.text = @"hehe";
+        message.text = @"专访张小龙：产品之上的世界观";
         WBWebpageObject *webpage = [WBWebpageObject object];
-        webpage.objectID = @"identifier1";
-        webpage.title = NSLocalizedString(@"gideon's blog", nil);
-        webpage.description = [NSString stringWithFormat:NSLocalizedString(@"分享网页内容简介-%.0f", nil), [[NSDate date] timeIntervalSince1970]];
-        webpage.thumbnailData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"image_2" ofType:@"jpg"]];
-        webpage.webpageUrl = @"http://qiuwanfujoe.github.io";
+        webpage.objectID = @"专访张小龙：产品之上的世界观objectID";
+        webpage.title = @"专访张小龙：产品之上的世界观";
+        webpage.description =  @"微信的平台化发展方向是否真的会让这个原本简洁的产品变得臃肿？在国际化发展方向上，微信面临的问题真的是文化差异壁垒吗？腾讯高级副总裁、微信产品负责人张小龙给出了自己的回复。";
+    webpage.thumbnailData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.baidu.com/img/bd_logo1.png"]];
+        webpage.webpageUrl = @"http://tech.qq.com/zt2012/tmtdecode/252.htm";
         message.mediaObject = webpage;
     
     return message;
